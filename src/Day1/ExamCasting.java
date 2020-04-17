cc3abd7451e169dec189ea7fafe52d0aa9761da8
whdydgur10 <whdydgur10@gmail.com>
2020-04-17 16:41:45 +0900

    1일차 자료 변환 예제

---
+++ src/Day1/ExamCasting.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class ExamCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = (int)1.23;
+		//명시적 자료형 변환 : int는 정수형이기 때문에 소수점을 버린다는 뜻으로 (int)를 넣는다.
+		double dnum = 1;
+		//자동 자료형 변환 : double은 실수형이기 때문에 1뒤에 소수점 .00이 붙는다.
+
+	}
+
+}
