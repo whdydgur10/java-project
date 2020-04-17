352dfd1d7c8cc9db94222c0ddf51cc14939354fe
whdydgur10 <whdydgur10@gmail.com>
2020-04-17 16:41:58 +0900

    1일차 테스트 예제

---
+++ src/Day1/test1.java
@@ -0,0 +1,16 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		System.out.print("정수를 입력하세요 : ");
+		System.out.println("입력한 정수는 : " + num);
+		scan.close();
+
+	}
+
+}
