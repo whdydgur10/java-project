bc750f62f471b1515b55eb9d5ee4b0b5eb2f9dd2
whdydgur10 <whdydgur10@gmail.com>
2020-04-20 17:28:44 +0900

    1일차 테스트 예제 수정

--- src/Day1/test1.java
+++ src/Day1/test1.java
@@ -16,22 +16,41 @@ public class test1 {
 			num = num2;
 			num2 = num3;		
 		}
-		System.out.println("두 값의 몫은 : " + num / num2 + "입니다.");
-		System.out.println("두 값의 나머지는 : " + num % num2 + "입니다.");
-		int a = num / num2;
-		int c = a;
+		//System.out.println("두 값의 몫은 : " + num / num2 + "입니다.");
+		//System.out.println("두 값의 나머지는 : " + num % num2 + "입니다.");
+		int quo = num / num2;
+		int newquo = quo;
 		int i = 1;
-		while (c > 1 ) {
-			System.out.println(i + "번째 몫은" + c + "입니다.");
+		int [] num4 = new int[32];
+		int b = 0;
+		while (newquo > 0 ) {
+			System.out.println(i + "번째 몫은" + newquo + "입니다.");
+			int rema = quo % num2;
+			System.out.println("나머지는 " + rema + "입니다.");
+			quo = newquo;
+			newquo = quo / num2;
+			num4[b] = rema;
+			b = i;
 			i++;
-			c = num / num2;
-			int b = num % num2;
-			System.out.println(b);
-			
 		}
-		String str = new String();
+		for(int j = 0; j <= b; j++) {
+			System.out.print(num4[j]);
+		}
+		
+		/*while(num != 1) {
+			num4[b++] = num % num2;
+			num = num / num2;
+		}
+		num4[b] = num;
+		for(int j = b; j > 0; j--) {
+			System.out.print(num4[j]);
+		}*/
+		//10진수를 2진수로 바꾸는 공식
+		
+		//for (int j = 0; j < num4.length; j++)
+		/*String str = new String();
 		str = scan.next();
-		System.out.println(str);
+		System.out.println(str);*/
 		scan.close();
 	}
 
